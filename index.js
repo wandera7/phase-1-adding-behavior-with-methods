a8a725d546386d12e21abd24408d267a1b56e40b
wandera7 <julius.wandera@student.moringaschool.com>
2022-06-18 12:43:57 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+class Cat{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+    speak(){
+        return this.sex==='male'?`It's me! ${this.name}, the parrot!`:`${this.name} says squawk!`
+    }
+}
